ede07f1c17673fe7f37bad63d08d6d88c69012a6
CurtL <curtis.lesieur@gmail.com>
2025-02-17 16:28:03 -0700

    added char count

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ def main():
     text = get_text(book_path)
     word_count = len(text.split())
 
-    print(word_count)
+    count_of_chars = char_count(text)
+
+    print(count_of_chars)
 
 
 def get_text(path):
     with open(path) as f:
         file_contents = f.read()
         return file_contents
+    
+def char_count(text):
+    text = text.lower()
+    count_of_chars = {}
+    for char in text:
+        if char in count_of_chars.keys():
+            count_of_chars[char] += 1
+        else:
+            count_of_chars[char] = 1
 
+    return count_of_chars
 
 if __name__=="__main__":
     main()
